751747651969c620ac2d54f4e3661e56c85025ff
Vladimir Petrigo <taenaru@gmail.com>
2019-08-04 20:19:58 +0300

    misc: Clean up

--- examples/intersection.rs
+++ examples/intersection.rs
@@ -1,20 +1,26 @@
+use std::fmt::Display;
+
+use num::complex::Complex;
+use num::traits::Float;
+use num::NumCast;
+
 use circle;
+use circle::Intesection::{Double, Single};
 
-use circle::Intesection::{Single, Double};
+fn dumb<T: Float + Display>(x: T, y: T) -> Complex<T> {
+    let _one: T = NumCast::from(1.0).unwrap();
+    let _two: T = NumCast::from(2.0).unwrap();
+    let div = _one + x.powi(2) + _two * x + y.powi(2);
+    let top_real = _one - x.powi(2) - y.powi(2);
+    let top_imag = -_two * y;
+    let y_adm = Complex::new(top_real / div, top_imag / div);
 
-fn dumb(x: f32, y: f32) {
-    let div = (1.0 + x.powi(2) + 2.0 * x + y.powi(2));
-    let top_real = (1.0 - x.powi(2) - y.powi(2));
-    let top_imag = (-2.0 * y);
-    let y_adm = top_real / div;
-    let b_adm = top_imag / div;
-
-    println!("{} {}", y_adm, b_adm);
+    y_adm.inv()
 }
 
 fn main() {
-    let c1 = circle::Circle::new(-0.38, 0.0, 0.621);
-    let c2 = circle::Circle::new(0.5, 0.0, 0.5);
+    let c1 = circle::Circle::new(-0.38_f32, 0.0, 0.621);
+    let c2 = circle::Circle::new(0.5_f32, 0.0, 0.5);
 
     let intersection = circle::circle_intersection(&c1, &c2);
 
@@ -23,8 +29,8 @@ fn main() {
             Single(p) => println!("Intersection point: x - {}, y - {}", p.0, p.1),
             Double(p1, p2) => {
                 println!("{:?} {:?}", p1, p2);
-                dumb(p1.0, p1.1);
-                dumb(p2.0, p2.1);
+                println!("{:.2}", dumb(p1.0, p1.1));
+                println!("{:.2}", dumb(p2.0, p2.1));
             }
         }
     }
